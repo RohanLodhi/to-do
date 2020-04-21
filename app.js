09c879774020538128f2f143d34e28abc0383671
RohanLodhi <rogth7@gmail.com>
2020-04-22 02:08:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var data;
+const input = document.querySelector('button');
+var key=0;
+if(localStorage.getItem('localKey') !== null){
+    key = localStorage.getItem('localKey');
+}
+else{
+    key = 0;
+}
+if(key !== 0){
+    console.log(key)
+    for(i=0;i<key;i++){
+        var li = document.createElement('li');
+        var ul = document.querySelector('ul');
+        li.appendChild(document.createTextNode(localStorage.getItem(i)));
+        ul.appendChild(li);
+    }
+}
+input.addEventListener('click', e => {
+    data = document.getElementById('data');
+    var li = document.createElement('li');
+    var ul = document.querySelector('ul');
+    if(data.value !== ""){
+        li.appendChild(document.createTextNode(data.value));
+        ul.appendChild(li);
+        localStorage.setItem(key, data.value);
+        key = parseInt(key)
+        console.log(key)
+        key+=1;
+        localStorage.setItem('localKey',key);
+        li.addEventListener('click', e => {
+            e.target.style.textDecoration = 'line-through';
+        });
+        li.addEventListener('dblclick', e =>{
+            e.target.remove();
+        });
+        data.value="";
+    };
+});
+const removeList = document.getElementById('delete')
+removeList.addEventListener('click',() => {
+    for(i=0;i<key;i++){
+        localStorage.removeItem(i);
+        var li =document.querySelector('li');
+        li.remove();
+    }
+    localStorage.removeItem('localKey')
+    console.log(localStorage.getItem('localKey'))
+    key = 0;
+})
