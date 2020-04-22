c9cbc9c15df86174a7186ff5d4d327654225a5ab
RohanLodhi <rogth7@gmail.com>
2020-04-22 20:18:20 +0530

    service worker

--- sw.js
+++ sw.js
@@ -7,20 +7,17 @@ var filesToCache = [
   '/manifest.webmanifest'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', (event) => {
+  console.log('👷', 'install', event);
+  self.skipWaiting();
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  console.log('👷', 'activate', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // console.log('👷', 'fetch', event);
+  event.respondWith(fetch(event.request));
 });
