18baef4418b130c6fe790ac16e694ed2d65497b6
RohanLodhi <rogth7@gmail.com>
2020-04-22 20:20:02 +0530

    cache

--- sw.js
+++ sw.js
@@ -21,3 +21,20 @@ self.addEventListener('fetch', function(event) {
   // console.log('👷', 'fetch', event);
   event.respondWith(fetch(event.request));
 });
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
