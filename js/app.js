51b1efb09ed07d43f6ba42f559088383e4e7766d
Rohan Lodhi <rogth7@gmail.com>
2020-04-22 16:13:50 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,16 +8,7 @@ window.onload = () => {
       navigator.serviceWorker.register('./sw.js');
     }
 }
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-}); 
+showInstallPromotion();
 if(localStorage.getItem('localKey') !== null){
     key = localStorage.getItem('localKey');
 }
